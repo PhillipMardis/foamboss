2dd3ccd564ae5d89c679d2ccdbb909cd246a4535
Phillip Mardis <pmardis@basecampcodingacademy.org>
2023-12-08 21:41:15 -0600

    updated ui. placeholder 'about' page

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
 ]
--- app/views.py
+++ app/views.py
@@ -5,3 +5,6 @@ from .models import Shape
 def home(request):
     shapes = Shape.objects.all()
     return render(request, 'home.html', {'shapes': shapes})
+
+def about(request):
+    return render(request, 'about.html', {})
