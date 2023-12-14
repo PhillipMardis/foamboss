4918e8f4f9b2651e17fcbeb0b830b676a361c90b
Phillip Mardis <pmardis@basecampcodingacademy.org>
2023-12-13 21:51:01 -0600

    completed login/logout system

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('design/', views.design, name='design'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 from .models import Shape
+
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
 def home(request):
@@ -11,3 +15,27 @@ def about(request):
 
 def design(request):
     return render(request, 'design.html', {})
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ("Login successful"))
+            return redirect('home')
+        else:
+            messages.success(request, ("Error logging in"))
+            return redirect('login')
+    else:
+        return render(request, 'login.html', {})
+
+        
+    
+    
+def logout_user(request):
+    logout(request)
+    messages.success(request, ("Logout success"))
+    return redirect('home')
+
